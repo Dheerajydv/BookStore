7f10ad7b493f0feda760e3845cfb919232d237b1
Dheerajydv <dy81035@gmail.com>
2024-09-28 19:04:23 +0530

    All Books page done

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,14 +1,44 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import Navbar from "../components/Navbar";
 import { AuthContext } from "../contexts/AuthContext";
-import Login from "./Login";
-import Signup from "./Signup";
+import axios from "axios";
 
 function Home() {
-  const { isAuthenticated } = useContext(AuthContext);
+  // const { isAuthenticated } = useContext(AuthContext);
+  const [allBooks, setAllBooks] = useState([]);
+  useEffect(() => {
+    axios.get("http://localhost:8000/api/v1/books/allbooks").then((res) => {
+      // console.log(res.data.data);
+      setAllBooks(res.data.data);
+    });
+  }, [setAllBooks]);
   return (
     <div>
       <Navbar />
+      <div>
+        <h1>ALL BOOKS</h1>
+        {allBooks[0]
+          ? allBooks.map((book) => (
+              <div key={book.title}>
+                <h2>{book.title}</h2>
+                <h3>{book.author}</h3>
+                <img src={book.cover} height={"100px"} alt="Book Cover" />
+                <div>
+                  <p>LIKES</p>
+                  <p>{book.likes}</p>
+                </div>
+                <div>
+                  <p>DISLIKES</p>
+                  <p>{book.disLikes}</p>
+                </div>
+                <div>
+                  <p>READ BY</p>
+                  <p>{book.readBy}</p>
+                </div>
+              </div>
+            ))
+          : "Loding"}
+      </div>
     </div>
   );
 }
