d17da81930ae4780cf10028f330f5a08b55e6b72
Dheerajydv <dy81035@gmail.com>
2025-04-14 19:31:24 +0530

    trying something

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,8 +8,6 @@ export default defineConfig({
     proxy: {
       "/api": {
         target: "https://bookstore-backend-1cyf.onrender.com",
-        secure: false,
-        changeOrigin: true,
       },
     },
   },
