3266158cc753903fcbf7123959d03b67f2c9e02d
Dheerajydv <dy81035@gmail.com>
2025-05-22 23:23:25 +0530

    V1 Release (Take 9)

--- backend/index.js
+++ backend/index.js
@@ -19,7 +19,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: true,
+    origin: "https://bookstore-1-9clk.onrender.com",
     optionsSuccessStatus: 200,
     credentials: true,
   })
